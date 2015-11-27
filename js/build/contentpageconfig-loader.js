c9c3ed45fb623073809cb5219a206154a0f6f1bb
Tamás Demeter-Haludka <yorirou@gmail.com>
2015-11-27 10:51:06 +0100

    Cache on custom page config

--- js/build/contentpageconfig-loader.js
+++ js/build/contentpageconfig-loader.js
@@ -15,6 +15,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module.exports = function(content) {
+	this.cacheable && this.cacheable();
+
 	var data = JSON.parse(content);
 	return "module.exports = {" +
 		Object.keys(data).map(function(path) {
