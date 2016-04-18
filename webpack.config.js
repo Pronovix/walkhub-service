d8f4c4894acc964d849a862d661f8df6a3bc2324
Tamás Demeter-Haludka <yorirou@gmail.com>
2016-04-18 13:03:47 +0200

    Adds markdown component loading.

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ var loaders = [
 	{ test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
 	{ test: /\.scss$/, loader: "style!css!sass?"+sassIncludePaths },
 	{ test: /\.sass$/, loader: "style!css!sass?indentedSyntax&"+sassIncludePaths },
-	{ test: /.*\.(gif|png|jpe?g|ico)$/i, loader: "file?name=[name]-[sha512:hash:hex:6].[ext]" }
+	{ test: /.*\.(gif|png|jpe?g|ico)$/i, loader: "file?name=[name]-[sha512:hash:hex:6].[ext]" },
+	{ test: /\.md*/, loader: "babel-loader!react-markdown!markdown" }
 ];
 
 if (contentpages) {
